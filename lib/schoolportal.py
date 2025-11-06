a225ed46078e81bce84d9ddf8ac880ba6b4344c3
Gerry <gerrywiegand@gmail.com>
2025-11-06 11:12:20 -0500

    created user superclass and teacher instance

--- lib/schoolportal.py
+++ lib/schoolportal.py
@@ -1,1 +1,10 @@
-#classes all go in here
+# classes all go in here
+#!/usr/bin/env python3
+class User:
+    def __init__(self, fist_name, last_name, email):
+        self.first_name = fist_name
+        self.last_name = last_name
+        self.email = email
+
+    def end_email(self):
+        return f"{self.first_name}.{self.last_name}@{self.email}"
